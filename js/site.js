c7b257c10e7c9dd7ba19d79634f7f6837c8cacfa
Heather Vandervecht <heathervv@users.noreply.github.com>
2017-02-24 22:28:04 -0500

    Feat new scroll (#3)
    * feat: reworked scroll
    
    * feat: remove scroll function on mobile. add google verification tag
    
    * feat: added CNAME

--- js/site.js
+++ js/site.js
@@ -1,14 +1,47 @@
-window.onscroll = function() {
-    var firstHero = document.getElementById("first-hero");
-    var lastHero = document.getElementById("last-hero");
+var setup;
 
-    var top = firstHero.getBoundingClientRect().top;
-    var bottom = lastHero.getBoundingClientRect().top;
-    
-    if(top == 0) {
-        window.scrollTo(0, bottom)
+function infiniteScroll() {
+    if(Modernizr.mq('(min-width: 1024px)')) {
+        if(setup != true) {
+            var origDocHeight = document.body.offsetHeight;
+
+            var container = document.getElementsByClassName('container')[0];
+
+            var clone = container.cloneNode(true);
+            document.body.appendChild(clone);
+            document.body.insertBefore(clone, document.body.childNodes[0]);
+
+            window.onscroll = function() {
+                var scrollWindowPos = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+                if(scrollWindowPos >= origDocHeight ) {
+                    window.scrollTo(0,0);
+                }
+                if(scrollWindowPos <= 0 ) {
+                    window.scrollTo(0, origDocHeight);
+                }
+            };
+
+            setup = true;
+        }
+    } else {
+        if(setup == true) {
+            setup = false;
+
+            window.onscroll = null;
+
+            var container = document.getElementsByClassName('container');
+
+            if(container[1]) {
+                while(container[1])
+                    container[1].parentNode.removeChild(container[1]);
+            }
+        }
     }
-    if(bottom <= 0) {
-        window.scrollTo(0,0);
-    }
+}
+
+infiniteScroll();
+
+window.onresize = function(event) {
+    infiniteScroll();
 };
