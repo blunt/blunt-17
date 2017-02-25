4dd2e5c27fe4d959315eed2d86a38869b9e8b1f3
Heather Vandervecht <heathervandervecht@gmail.com>
2017-02-24 21:46:25 -0500

    feat: reworked scroll

--- js/site.js
+++ js/site.js
@@ -1,14 +1,20 @@
-window.onscroll = function() {
-    var firstHero = document.getElementById("first-hero");
-    var lastHero = document.getElementById("last-hero");
+(function() {
+    var origDocHeight = document.body.offsetHeight;
 
-    var top = firstHero.getBoundingClientRect().top;
-    var bottom = lastHero.getBoundingClientRect().top;
-    
-    if(top == 0) {
-        window.scrollTo(0, bottom)
-    }
-    if(bottom <= 0) {
-        window.scrollTo(0,0);
-    }
-};
+    var container = document.getElementById('container');
+
+    var clone = container.cloneNode(true);
+    document.body.appendChild(clone);
+    document.body.insertBefore(clone, document.body.childNodes[0]);
+
+    window.onscroll = function() {
+        var scrollWindowPos = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+        if(scrollWindowPos >= origDocHeight ) {
+            window.scrollTo(0,0);
+        }
+        if(scrollWindowPos <= 0 ) {
+            window.scrollTo(0, origDocHeight);
+        }
+    };
+})();
