9018621532b53eb642246a19caac93d815cb0d7d
Heather Vandervecht <heathervandervecht@gmail.com>
2017-02-24 22:19:52 -0500

    feat: remove scroll function on mobile. add google verification tag

--- js/site.js
+++ js/site.js
@@ -1,20 +1,47 @@
-(function() {
-    var origDocHeight = document.body.offsetHeight;
+var setup;
 
-    var container = document.getElementById('container');
+function infiniteScroll() {
+    if(Modernizr.mq('(min-width: 1024px)')) {
+        if(setup != true) {
+            var origDocHeight = document.body.offsetHeight;
 
-    var clone = container.cloneNode(true);
-    document.body.appendChild(clone);
-    document.body.insertBefore(clone, document.body.childNodes[0]);
+            var container = document.getElementsByClassName('container')[0];
 
-    window.onscroll = function() {
-        var scrollWindowPos = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+            var clone = container.cloneNode(true);
+            document.body.appendChild(clone);
+            document.body.insertBefore(clone, document.body.childNodes[0]);
 
-        if(scrollWindowPos >= origDocHeight ) {
-            window.scrollTo(0,0);
+            window.onscroll = function() {
+                var scrollWindowPos = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+                if(scrollWindowPos >= origDocHeight ) {
+                    window.scrollTo(0,0);
+                }
+                if(scrollWindowPos <= 0 ) {
+                    window.scrollTo(0, origDocHeight);
+                }
+            };
+
+            setup = true;
         }
-        if(scrollWindowPos <= 0 ) {
-            window.scrollTo(0, origDocHeight);
+    } else {
+        if(setup == true) {
+            setup = false;
+
+            window.onscroll = null;
+
+            var container = document.getElementsByClassName('container');
+
+            if(container[1]) {
+                while(container[1])
+                    container[1].parentNode.removeChild(container[1]);
+            }
         }
-    };
-})();
+    }
+}
+
+infiniteScroll();
+
+window.onresize = function(event) {
+    infiniteScroll();
+};
